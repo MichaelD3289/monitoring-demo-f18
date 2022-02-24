afb3690bcae229f033cd1bd3405044fed0be4d8b
Michael Drummond <michael.drummond328@gmail.com>
2022-02-24 12:56:52 -0700

    trying again

--- server/index.js
+++ server/index.js
@@ -24,14 +24,23 @@ app.get('/', (req, res) => {
 let students = []
 
 app.post('/api/student', (req, res)=>{
-    let {name} = req.body
-    name = name.trim()
+  let {name} = req.body
+  name = name.trim()
 
-    students.push(name)
+  const index = students.findIndex(studentName=> studentName === name)
 
-    rollbar.log('student was added successfully', {author: 'Scott', type: 'manual', student: name})
+  if(index === -1 && name !== ''){
+      students.push(name)
+      rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+      res.status(200).send(students)
+  } else if (name === ''){
+      rollbar.error('No name given')
+      res.status(400).send('must provide a name.')
+  } else {
+      rollbar.error('student already exists')
+      res.status(400).send('that student already exists')
+  }
 
-    res.status(200).send(students)
 })
 
 app.use(rollbar.errorHandler())
