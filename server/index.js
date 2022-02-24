0f80e21d8db166f5a1297b26cc6f0e101c3eb202
Michael Drummond <michael.drummond328@gmail.com>
2022-02-24 12:12:59 -0700

    added rollbar

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const path = require('path');
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'b05939a25b5948ae8b35c5f1652e4c8e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express();
 
 app.get('/', (req, res) => {
